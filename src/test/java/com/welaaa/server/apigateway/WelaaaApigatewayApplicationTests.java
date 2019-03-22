2bc190380ceef49c23ee04925cad9fa4ccb3d0dd
강형구 <hyunggoo.kang@influential.co.kr>
2019-03-22 13:35:48 +0900

    Initial commit

---
+++ src/test/java/com/welaaa/server/apigateway/WelaaaApigatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.welaaa.server.apigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WelaaaApigatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
